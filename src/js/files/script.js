0d97c58aabc5e8405edd18c2f597d406703f8e6d
osdal <osdal67@gmail.com>
2023-06-04 11:36:05 +0300

    Готовая верстка главной, архива и поста

---
+++ src/js/files/script.js
@@ -0,0 +1,13 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+
+// let topSectionSecondRow = document.querySelector('.top-section__secondRow');
+
+// let width = topSectionSecondRow.offsetWidth;
+
+// topSectionSecondRow.style.height = width + 'px';
+
+// console.log(width);
